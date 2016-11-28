7a0294a3e6712f67be030cb31748a4b0fba244ec
PAULO RICARDO RUBE DE ALMEIDA <paulo@actio.net.br>
2016-11-28 16:39:31 -0200

    Create helps.js

---
+++ helps.js
@@ -0,0 +1,8 @@
+// contar quantidade de elementos
+var tamanho = $("input[name="elemento"]").size();
+
+// adicionar atributos
+$("#elemento").attr("id","idelemento");
+
+//alterar valor de data-id;
+$("#elemento").data ("id","valorReceber");
