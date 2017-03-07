023f5592c9270f0e37efebee12737bb95f448196
PAULO RICARDO RUBE DE ALMEIDA <paulo@actio.net.br>
2017-03-07 11:02:47 -0300

    Create tabeladePrecoAntiga.js

---
+++ tabeladePrecoAntiga.js
@@ -0,0 +1,25 @@
+	// Inicio Montagem Array Tabela de Preco
+	/*	while (true)
+		{
+			$cLegenda = $this->getSX6("TABELAPRECO".$i);
+
+			if($cLegenda == '')
+			{
+				break;
+			}
+			else
+			{
+				$aTabelaPrecos[$i]['desc']  = $cLegenda;
+				$aTabelaPrecos[$i]['value'] = $i;
+			}
+			$i++;
+		}
+
+		foreach ($aTabelaPrecos as $k => $v)
+		{
+			if($k == $aRet["SC5_TabelaPreco"])
+			{
+				$aTabelaPrecos[$k]['selected'] = 'selected="selected"';
+			}
+	}*/
+		//Final Montagem Array Tabela de Preco
